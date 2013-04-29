c0a5467a6d4b26e8cafce136404c6a3d50a2e522
Tomasz Lenarcik <tomek.lenarcik@gmail.com>
2013-04-29 19:42:47 +0200

    turn into smart package

---
+++ package.js
@@ -0,0 +1,13 @@
+
+Package.describe({
+    summary: "A toolset for action buttons creation",
+});
+
+Package.on_use(function (api) {
+    //TODO: remove underscore dependency?
+    api.use(['spark', 'underscore'], 'client');
+
+    // JS code
+    api.add_files('actions.js', 'client');
+    api.add_files('actions.js', 'client');
+});
