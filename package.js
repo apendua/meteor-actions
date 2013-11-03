825646dbafe669e794e53d7faf8fb23bf5f64491
apendua <apendua@gmail.com>
2013-11-03 18:41:42 +0100

    Small fixes

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-    api.use(['spark', 'templating'], 'client');
+    api.use(['spark', 'templating', 'handlebars'], 'client');
     api.use(['underscore', 'ejson'], ['client', 'server']);
 
     api.add_files('actions.js', ['client', 'server']);
@@ -12,4 +12,7 @@ Package.on_use(function (api) {
     
     api.add_files('helpers.html', 'client');
     api.add_files('helpers.js', 'client');
+
+    if (api.export)
+        api.export('Actions');
 });
